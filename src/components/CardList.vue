34a840b96fcde083effc72bfdedb0fce49ec1c4f
otsairaze <ssavelevdenchikk@gmail.com>
2024-11-11 21:58:33 +0200

    initial project

---
+++ src/components/CardList.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Card from './Card.vue'
+
+const onClickAdd = () => {
+  alert('Добавить')
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-10">
+    <Card
+      image-url="/sneakers/sneakers-2.jpg"
+      title="Мужские Кроссовки Nike Blazer Mid Suede"
+      :price="12999"
+      :is-added="true"
+      :is-favorite="false"
+      :onClickAdd="onClickAdd"
+    />
+  </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="flex justify-between border-b border-slate-300 px-10 py-8">
+    <div class="flex items-center gap-4">
+      <img src="/logo.png" alt="Logo" class="w-10 h-10" />
+      <div>
+        <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
+        <p class="text-slate-500">Магазин лучших кроссовок</p>
+      </div>
+    </div>
+
+    <ul class="flex items-center gap-10">
+      <li class="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black">
+        <img src="/cart.svg" alt="Cart" />
+        <b>1205. руб</b>
+      </li>
+
+      <li class="flex items-center gap-3 cursor-pointer text-gray-500">
+        <img src="/heart.svg" alt="Cart" />
+        <span>Закладки</span>
+      </li>
+
+      <li class="flex items-center gap-3 cursor-pointer text-gray-500">
+        <img src="/profile.svg" alt="Cart" />
+        <span>Профиль</span>
+      </li>
+    </ul>
+  </header>
+</template>
